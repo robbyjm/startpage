cb5afb103af386f25c397637650708552781a2a6
robbyjm <robbyj.manis@gmail.com>
2019-09-11 18:45:06 -0400

    initial nightly

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?zip=94040,us&units=imperial&APPID=052f4bae1974a3dc50350d7c1c628b67').then(function (response) {
+fetch('https://api.openweathermap.org/data/2.5/weather?zip=94040,us&units=imperial&APPID=YOURAPPIDHERE').then(function (response) {
     return response.json();
 })
     .then(function (myJson) {
